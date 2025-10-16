15acbe39f058c0f46f634232b0f6c0b91c03f018
Alan Daiki Suga <110938759+daikisuga@users.noreply.github.com>
2025-10-16 20:37:14 -0300

    Update task validation tests with status parameter

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,18 +1,25 @@
 import pytest
 from datetime import datetime, timedelta
 from task_manager.task import Task, Priority, Status
+
 def test_task_valida():
     prazo = datetime.now() + timedelta(days=1)
-    task = Task(None, "Estudar", "Python", Priority.ALTA, prazo)
-    task.validar() # Nao deve lancar erro
+    task = Task(None, "Estudar", "Python", Priority.ALTA, Status.PENDENTE, prazo)
+    
+    task.validar(task.titulo, task.prazo)
+    
     assert task.titulo == "Estudar"
+
 def test_titulo_curto_invalido():
     prazo = datetime.now() + timedelta(days=1)
-    task = Task(None, "AB", "Desc", Priority.BAIXA, prazo)
+    task = Task(None, "AB", "Desc", Priority.BAIXA, Status.PENDENTE, prazo)
+    
     with pytest.raises(ValueError):
-        task.validar()
+        task.validar(task.titulo, task.prazo)
+
 def test_prazo_passado():
-    prazo = datetime.now - timedelta(days=1)
-    task = Task(None,"Rapaaaaaaiz","Ta losco",Priority.MEDIA,prazo)
+    prazo = datetime.now() - timedelta(days=1) 
+    task = Task(None, "Rapaaaaaaiz", "Ta losco", Priority.MEDIA, Status.PENDENTE, prazo)
+    
     with pytest.raises(ValueError):
-        task.validar()
+        task.validar(task.titulo, task.prazo)
